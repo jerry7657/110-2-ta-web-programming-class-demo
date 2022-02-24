f7e37188d9fa7c767d99363a12970bb9c7fa96be
jerry7657 <56625625+jerry7657@users.noreply.github.com>
2022-02-24 19:16:39 +0800

    week 1

---
+++ week_1_markdown_previewer/class_demo/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+
+function App() {
+  
+  return (
+    <main>
+      <h1>Markdown Previewer</h1>
+    </main>
+  )
+  
+}
+
+export default App
